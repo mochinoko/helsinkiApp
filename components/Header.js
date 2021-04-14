b7504b5beeadf524a7ae507b87b09d552be5ecf5
Yuki Sugiyama <35254255+mochinoko@users.noreply.github.com>
2021-04-14 18:40:36 +0300

    adding Header component

---
+++ components/Header.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default function HeaderTop() {
+  return (
+    <View style={styles.container}>
+     <Header 
+     leftComponent={{ icon: 'menu', color: '#fff' }}
+     centerComponent={{ text: 'Helsinki Events Search', style: { color: '#fff' } }} 
+     rightComponent={{ icon: 'home', color: '#fff' }}
+     />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+       // flex: 1,
+        backgroundColor: '#fff',
+        //alignItems: 'center',
+        justifyContent: 'flex-end',
+        width: '100%',
+          },
+    })
