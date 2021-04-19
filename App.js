268f7801b145043bcccbab7f34f1f6065f4e6364
Yuki Sugiyama <35254255+mochinoko@users.noreply.github.com>
2021-04-19 21:17:36 +0300

    modified links

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LandingPage from './components/LandingPage';
+import AppNavigation from "./screens/AppNavigation";
+import { NavigationContainer } from '@react-navigation/native';
 
 
 export default function App() {
   return (
-    <LandingPage/>
-
+      <AppNavigation />
   );
 }
 
